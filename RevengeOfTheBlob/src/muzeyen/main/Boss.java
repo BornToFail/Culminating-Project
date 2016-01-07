7a5d2a8dacd3f98944fb81ce6711574248b034c2
Aden Downes <aden0298@gmail.com>
2016-01-07 13:51:10 -0500

    .

--- RevengeOfTheBlob/src/muzeyen/main/Boss.java
+++ RevengeOfTheBlob/src/muzeyen/main/Boss.java
@@ -1,6 +1,7 @@
 package muzeyen.main;
 
 import java.awt.Graphics;
+
 import java.awt.image.BufferedImage;
 
 
@@ -12,6 +13,26 @@ import java.awt.image.BufferedImage;
  * @author 342643103
  *
  */
+//Start a counter when the boss level starts.
+/*Timer T=new Timer();
+T.scheduleAtFixedRate(new TimerTask() {         
+                @Override
+                public void run() {
+                    runOnUiThread(new Runnable()
+                    {
+
+                        public void run()
+                        {
+                            countdown.setText(""+count);
+                            count++;                
+                        }
+                    });
+                }
+            }, 1000, 1000);
+*/
+
+//T.cancel();
+
 public class Boss {
 	/**
 	 * 
@@ -19,22 +40,18 @@ public class Boss {
 	private double X;
 	private double Y;
 	
+	private double XSpeed;
+	private double YSpeed;
+	
 	private BufferedImage boss;
 	
-	private void Moves(){
-	//Boss' movement method	
-		
-	
-		
-		
-	}
 	
 	public Boss(double X, double Y, Game game) {
 		this.X=X;
 		this.Y=Y;
 		
 		SpriteSheet ss = new SpriteSheet(game.getSpriteSheet());
-		//boss=ss.grabImage(1, 5, 32, 32);
+		boss=ss.grabImage(5, 1, 32, 32);
 	}
 	
 	public double getX(){
@@ -50,6 +67,31 @@ public class Boss {
 		this.Y = Y;
 	}
 	
+	public void setXSpeed(double XSpeed){
+		this.XSpeed = XSpeed;
+	}
+	public void setVelY(double YSpeed){
+		this.YSpeed = YSpeed;
+	}
+	
+	private void Moves(){
+		//Boss' movement method	
+			//If boss speed is fast
+				//Boss is visible
+		
+			//If boss speed is slow
+				//Boss is only invisible when user shoots
+		
+				
+		
+			
+			
+		}
+	
+	private void Shooting(){
+		//If boss shoots, player freezes for 2 seconds
+	}
+		
 
 }
 
